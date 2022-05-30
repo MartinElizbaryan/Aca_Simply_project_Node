f0f3fbc4811cfa6524bde14a4ad5478e93c30741
Martin ELizbaryan <martin.elizbaryan@gmail.com>
2022-05-30 19:26:51 +0400

    comment get user with messages

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const usersController = require("../controllers/users.controller");
 
 /* GET users listing. */
 router.get("/:id", usersController.find);
-router.get("/:id/messages", usersController.findWithMessages);
+// router.get("/:id/messages", usersController.findWithMessages);
 router.put("/:id", usersController.update);
 router.patch("/:id/add-money", usersController.addMoney);
 
