e6816154292c5b657c111daa33bb469de434ab83
Martin ELizbaryan <martin.elizbaryan@gmail.com>
2022-05-30 19:26:28 +0400

    comment get user with messages

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -17,29 +17,29 @@ exports.find = async function (req, res) {
   res.send(user);
 }
 
-exports.findWithMessages = async function (req, res) {
-  const id = +req.params.id;
-  const user = await prisma.user.findFirst({
-    where: {
-      id
-    },
-    select: {
-      name: true,
-      surname: true,
-      messages_from: {
-        where: {
-          to_id: 2 // should be auth id
-        },
-      },
-      messages_to: {
-        where: {
-          from_id: 2 // should be auth id
-        },
-      },
-    },
-  })
-  res.send(user);
-}
+// exports.findWithMessages = async function (req, res) {
+//   const id = +req.params.id;
+//   const user = await prisma.user.findFirst({
+//     where: {
+//       id
+//     },
+//     select: {
+//       name: true,
+//       surname: true,
+//       messages_from: {
+//         where: {
+//           to_id: 2 // should be auth id
+//         },
+//       },
+//       messages_to: {
+//         where: {
+//           from_id: 2 // should be auth id
+//         },
+//       },
+//     },
+//   })
+//   res.send(user);
+// }
 
 exports.update = async function (req, res) {
   const {name, surname, phone} = req.body;
