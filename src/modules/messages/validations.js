bac485f2d2fe08d48d54f45e6e6e221c969c5abb
Martin ELizbaryan <martin.elizbaryan@gmail.com>
2022-06-04 17:19:15 +0400

    add validation to params id integer positive

--- src/modules/messages/validations.js
+++ src/modules/messages/validations.js
@@ -3,13 +3,13 @@ import Joi from "joi"
 export default {
   getAllMessagesSchema: {
     params: Joi.object({
-      id: Joi.number().integer().required(),
+      id: Joi.number().integer().positive().required(),
     }),
   },
 
   createMessageSchema: {
     params: Joi.object({
-      id: Joi.number().integer().required(),
+      id: Joi.number().integer().positive().required(),
     }),
     body: Joi.object({
       message: Joi.string().required(),
