8f8a71e867d0ec57955e941d2b43e40b4c154bef
Martin ELizbaryan <martin.elizbaryan@gmail.com>
2022-06-20 16:41:52 +0400

    messgage is_seen

--- src/modules/messages/routes.js
+++ src/modules/messages/routes.js
@@ -4,11 +4,12 @@ import auth from "../../middlewares/auth.middleware.js"
 import validate from "../../middlewares/validate.middleware.js"
 import validations from "./validations.js"
 
-const { getAllMessagesSchema, createMessageSchema } = validations
+const { getAllMessagesSchema, createMessageSchema, seenMessageSchema } = validations
 
 const router = Router()
 
 router.get("/:id", auth, validate(getAllMessagesSchema), service.getAllMessages)
 router.post("/:id", auth, validate(createMessageSchema), service.createMessage)
+router.patch("/:id", auth, validate(seenMessageSchema), service.seenMessage)
 
 export { router as messagesRoutes }
