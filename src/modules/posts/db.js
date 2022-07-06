2ff3d6d0f1085985931376f51db484807369d394
Martin ELizbaryan <martin.elizbaryan@gmail.com>
2022-07-06 10:47:43 +0400

    posts db changes

--- src/modules/posts/db.js
+++ src/modules/posts/db.js
@@ -225,6 +225,9 @@ export const getPostWithQuestionsByIdDB = async (id) => {
         id: +id,
       },
       include: {
+        user: true,
+        images: true,
+        confirmer: true,
         questions: {
           include: {
             answers: true,
